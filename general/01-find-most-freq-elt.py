53431b98d5cdfbe09d8f1487ae2f8e29794f4422
Coin Data School <coindataschool@gmail.com>
2026-03-07 15:50:01 +0700

    use a list of strings instead of numbers

--- general/01-find-most-freq-elt.py
+++ general/01-find-most-freq-elt.py
@@ -1,11 +1,26 @@
-# find the most frequent element in a list
+"""
+Find the most frequent element in a list
+"""
 
-# method 1: pass frequency count method to max's key arg
-a = [1,2,3,1,2,3,2,2,4,5,1]
-max(set(a), key=a.count)
-# a.count(2)
-
-# method 2: use Counter
+# method 1. use Counter
 from collections import Counter
+
+a = [
+    "George",
+    "David",
+    "Anna",
+    "George",
+    "David",
+    "Anna",
+    "David",
+    "David",
+    "Hector",
+    "Jin",
+    "George",
+]
 cnt = Counter(a)
-cnt.most_common(1) # [(element, frequency), ...]
+cnt.most_common(1)  # [(element, frequency), ...]
+
+# method 2. pass the list's count method to max()
+most_freq_elt = max(set(a), key=a.count)
+(most_freq_elt, a.count("David"))
